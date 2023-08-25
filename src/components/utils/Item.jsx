64ccabc2d0dc918b92e4cb735e49fb08d6782f40
kaju111 <saikiakaju275@gmail.com>
2023-08-25 13:04:17 +0530

    3rd commit

--- src/components/utils/Item.jsx
+++ src/components/utils/Item.jsx
@@ -1,31 +1,46 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+
 import { StarIcon, ShoppingBagIcon } from "@heroicons/react/24/solid";
-import { setAddItemToCart } from "../../app/CartSlice";
+import { setAddItemToCart, setOpenCart } from "../../app/CartSlice";
 
-function Item({
-  ifExists,id,color,shadow,title,text,img,btn,rating,price,
-}) {
-
-  const dispatch = useDispatch()
+const Item = ({
+  ifExists,
+  id,
+  color,
+  shadow,
+  title,
+  text,
+  img,
+  btn,
+  rating,
+  price,
+}) => {
+  //   console.log(id)
+  const dispatch = useDispatch();
 
   const onAddToCart = () => {
-      const item = { id, title, text, img, color, shadow, price}
+    const item = { id, title, text, img, color, shadow, price };
 
-      dispatch(setAddItemToCart(item))
+    dispatch(setAddItemToCart(item));
+  };
 
-    }
+  const onCartToggle = () => {
+    dispatch(setOpenCart({
+        cartState: true
+    }))
+}
 
   return (
     <>
       <div
         className={`relative bg-gradient-to-b ${color} ${shadow} grid items-center ${
-          ifExists ? "justify-start" : "justify-center"
-        } rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full hover:scale-110`}
+          ifExists ? "justify-items-start" : "justify-items-center"
+        } rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full hover:scale-105`}
       >
         <div
           className={`grid items-center ${
-            ifExists ? "justify-start" : "justify-center"
+            ifExists ? "justify-items-start" : "justify-items-center"
           }`}
         >
           <h1 className="text-slate-200 text-xl lg:text-lg md:text-base font-medium filter drop-shadow">
@@ -36,13 +51,12 @@ function Item({
           </p>
 
           <div className="flex items-center justify-between w-28 my-2">
-            <div className="flex items-center bg-white/80 px-1 rounded blur-effect-theme">
+            <div className="flex items-center bg-white/80  px-1 rounded blur-effect-theme">
               <h1 className="text-black text-sm font-medium">${price}</h1>
             </div>
-
             <div className="flex items-center gap-1">
-              <StarIcon className="icon-style w-5 h-5 md:h-4 md:w-4" />
-              <h1 className="md:text-sm font-normal text-slate-100 ">
+              <StarIcon className="icon-style w-5 h-5 md:w-4 md:h-4" />
+              <h1 className="md:text-sm font-normal text-slate-100">
                 {rating}
               </h1>
             </div>
@@ -59,6 +73,7 @@ function Item({
             <button
               type="button"
               className="bg-white/90 blur-effect-theme button-theme px-2 py-1 shadow shadow-sky-200 text-sm text-black"
+              onClick={()=> {onAddToCart(); onCartToggle();}}
             >
               {btn}
             </button>
@@ -72,7 +87,7 @@ function Item({
           <img
             src={img}
             alt={`img/item-img/${id}`}
-            className={` transition-theme hover:-rotate-12 ${
+            className={`transitions-theme hover:-rotate-12 ${
               ifExists
                 ? "h-auto w-64 lg:w-56 md:w-48 -rotate-[35deg]"
                 : "h-36 w-64"
@@ -82,6 +97,6 @@ function Item({
       </div>
     </>
   );
-}
+};
 
 export default Item;
